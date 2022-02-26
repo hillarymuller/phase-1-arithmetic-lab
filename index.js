53ef0cffd03bf4f4aeae6b8450d8d57b0a821c2e
Hillary Muller <hillaryaward@gmail.com>
2022-02-26 14:05:32 -0500

    commit tree bdc6ea963945c96301313c8688b4671c71e29ce7

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply;
+let random = Number.parseInt(Math.random() + 1 , 10);
+random;
+let num3 = 32;
+let num4 = 7;
+let mod = num3 % num4;
+mod;
+let max = Math.max(5, 10, 15, 20);
+max;
